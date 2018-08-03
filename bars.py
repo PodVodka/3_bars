308e5cbd6b984b337b0b6a6430ad02cab2bd34aa
PodVodka <40150015+PodVodka@users.noreply.github.com>
2018-08-04 02:41:48 +0300

    Update bars.py

--- bars.py
+++ bars.py
@@ -1,61 +1,80 @@
 import json
 import argparse
-import math
+from math import sqrt
 
 
-def load_data(filepath):
+def load_from_json(filepath):
 
-    with open(filepath, 'r') as f:
-        bars_list = json.load(f)
-    return bars_list
+    with open(filepath, 'r') as file_object:
+        try:
+            return json.load(file_object)
+        except json.decoder.JSONDecodeError:
+            return None
 
 
 def get_biggest_bar(bars_list):
-    biggest_bar = max(bars_list['features'], key=lambda k: k['properties']['Attributes']['SeatsCount'])
-    return biggest_bar['properties']['Attributes']['Name']
+    return max(bars_list, key=get_seats_count)
 
 
 def get_smallest_bar(bars_list):
-    smallest_bar = min(bars_list['features'], key=lambda k: k['properties']['Attributes']['SeatsCount'])
-    return smallest_bar['properties']['Attributes']['Name']
+    return min(bars_list, key=get_seats_count)
+
+
+def get_seats_count(bar):
+    return bar['properties']['Attributes']['SeatsCount']
 
 
 def get_distance(bar, longitude, latitude):
     bar_long = bar['geometry']['coordinates'][0]
     bar_lat = bar['geometry']['coordinates'][1]
-    radius = 6371
-
-    dlat = math.radians(bar_lat-latitude)
-    dlon = math.radians(bar_long-longitude)
-    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(latitude)) \
-        * math.cos(math.radians(bar_lat)) * math.sin(dlon/2) * math.sin(dlon/2)
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    distance = radius * c
-
-    return distance
+    return sqrt((bar_long - longitude) ** 2 + (bar_lat - latitude) ** 2)
 
 
 def get_closest_bar(bars_list, longitude, latitude):
-    closest_bar = min(bars_list['features'], key=lambda k: get_distance(k, longitude, latitude))
-    return closest_bar['properties']['Attributes']['Name']
+    return min(bars_list, key=lambda k: get_distance(k, longitude, latitude))
+
+
+def input_coordinate(message=''):
+    try:
+        return float(input(message))
+    except ValueError:
+        return None
+
+
+def get_seats_count(bar):
+    return bar['properties']['Attributes']['SeatsCount']
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('file_path', help='You may download it here: https://devman.org/fshare/1503831681/4/')
+    parser.add_argument(
+        'file_path',
+        help='Ссылка для скачивания: https://devman.org/fshare/1503831681/4/'
+    )
     args = parser.parse_args()
     return args
 
 
-if __name__ == '__main__':
-    bars_list = load_data(parse_args())
-    biggest = get_biggest_bar(bars_list)
-    print('Самый вместительный бар:', biggest)
-    smallest = get_smallest_bar(bars_list)
-    print('Бар с наименьшим количеством посадочных мест:', smallest)
-    print('Чтобы найти ближейший бар укажите Ваши координаты')
-    longitude = float(input('Введите значение долготы:'))
-    latitude = float(input('Введите значение широты:'))
-    closest = get_closest_bar(bars_list, longitude, latitude)
+def get_bar_name(bar_dict):
+    return bar_dict['properties']['Attributes']['Name']
 
-    print('Ближайший к Вам бар:', closest)
+
+if __name__ == '__main__':
+    bars_filepath = parse_args().file_path
+    try:
+        bars = load_from_json(bars_filepath)['features']
+    except FileNotFoundError:
+        exit('Файл не найден')
+    if bars is None:
+        exit('Файл пуст')
+    print('Чтобы найти ближайший бар укажите Ваши координаты')
+    latitude = input_coordinate('Введите значение широты:')
+    longitude = input_coordinate('Введите значение долготы:')
+    if longitude is None or latitude is None:
+        exit('Введены некорректные данные. Программа завершена')
+    print('Самый большой бар:', get_bar_name(get_biggest_bar(bars)))
+    print('Самый маленький бар:', get_bar_name(get_smallest_bar(bars)))
+    print(
+        'Ближайший к Вам бар:',
+        get_bar_name(get_closest_bar(bars, longitude, latitude))
+    )
